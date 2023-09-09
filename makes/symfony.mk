9b1ea54446fd6a3657abbc5acde6fa8c1b1f76f3
Fan2Shrek <pierre27.ambroise@gmail.com>
2023-09-09 18:01:25 +0200

    Add @ to symfony make

--- makes/symfony.mk
+++ makes/symfony.mk
@@ -3,11 +3,11 @@ SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 include $(SELF_DIR)binaries/docker.mk
 
 database:
-	$(php) bin/console doctrine:database:create
-	$(php) bin/console doctrine:schema:update -f --complete
-	$(php) bin/console doctrine:fixture:load -n
+	@$(php) bin/console doctrine:database:create
+	@$(php) bin/console doctrine:schema:update -f --complete
+	@$(php) bin/console doctrine:fixture:load -n
 
 database-drop:
-	$(php) bin/console doctrine:database:drop -f
+	@$(php) bin/console doctrine:database:drop -f
 
 database-reset: database-drop database
