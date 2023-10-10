a9e24b7ebfe9e37d1c38435b515c0b63162779e1
Batmaxou <maximebatista.lycee@gmail.com>
2023-10-10 14:25:12 +0200

    fix @ boing.mk

--- makes/boing.mk
+++ makes/boing.mk
@@ -3,7 +3,7 @@ SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 include $(SELF_DIR)binaries/php.mk
 
 boing:
-	$(composer) require friendsofphp/php-cs-fixer --dev
-	$(composer) require phpstan/phpstan --dev
+	@$(composer) require friendsofphp/php-cs-fixer --dev
+	@$(composer) require phpstan/phpstan --dev
 
 	@echo "\n$(GREEN)--> Lint packages installed <--$(RESET)\n"
