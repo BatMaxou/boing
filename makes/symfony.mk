99ca0e9c0967235f6a3791f40ac2f5bea780378e
Fan2Shrek <pierre27.ambroise@gmail.com>
2023-11-01 18:04:50 +0100

    Add more database target for symfony

--- makes/symfony.mk
+++ makes/symfony.mk
@@ -1,17 +1,23 @@
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-include $(SELF_DIR)lint/symfony.mk
 include $(SELF_DIR)boing.mk
+include $(SELF_DIR)lint/symfony.mk
 
-database:
+database-create:
 	@$(php) bin/console doctrine:database:create
-	@$(php) bin/console doctrine:schema:update -f --complete
-	@$(php) bin/console doctrine:fixture:load -n
 
 database-drop:
 	@$(php) bin/console doctrine:database:drop -f
 
+database-update:
+	@$(php) bin/console doctrine:schema:update -f --complete
+
+database-fixtures:
+	@$(php) bin/console doctrine:fixture:load -n
+
+database: database-create database-update database-fixtures
+
 database-reset: database-drop database
 
 install:
-	@$(MAKE) do-vendor
+	@$(MAKE) vendor
