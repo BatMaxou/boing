ede7dbc2e806c3325c8d54c5f12766734e78e4d9
Maxime <maxime@maximebatista.lycee@gmail.com>
2023-11-07 20:39:15 +0100

    change position of phpfixer to php.mk

--- makes/lint/symfony.mk
+++ makes/lint/symfony.mk
@@ -2,10 +2,3 @@ include $(SELF_DIR)lint/php.mk
 
 PHPSTAN_CODE_PATH ?= src
 PHPSTAN_CONFIGURATION_FILE ?= $(SELF_DIR)../lint/phpstan/symfony.neon
-PHP_CS_FIXER_CONFIGURATION_FILE ?= $(SELF_DIR)../lint/php-cs-fixer/.php-cs-fixer.php
-
-fixcs: ## Lint - PHPCs - Fixes coding standards
-	@$(phpcsfixer) fix --config=$(PHP_CS_FIXER_CONFIGURATION_FILE)
-
-phpcs: ## Lint - PHPCs - Check coding standards
-	@$(phpcsfixer) fix --config=$(PHP_CS_FIXER_CONFIGURATION_FILE) --dry-run
